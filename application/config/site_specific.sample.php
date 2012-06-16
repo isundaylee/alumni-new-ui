2adb781ca61482e633788f4fa69bb34f7b1e3847
李嘉浩 <isundaylee.reg@gmail.com>
2012-06-15 22:21:53 -0400

    Organization commit

--- application/config/site_specific.sample.php
+++ application/config/site_specific.sample.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['base_url']	= 'http://localhost/~Sunday/alumni/';
+$config['base_url']	= 'http://localhost/~Sunday/alumni/alumni-new-ui/';
 
 $db['default']['port'] = '3306';
 $db['default']['hostname'] = '127.0.0.1';
